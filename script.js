ca795eceabbc30651a8b02b9f8041d04c1f2d408
SaadiaAjbbar <ajbbarsaadia6@gmail.com>
2025-10-29 23:19:43 +0100

    declaration de variable score et initialiser le avec 0

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const list = ["1", "3", "2", "7", "3", "1", "4", "4", "8", "5", "6", "6", "2", "7", "5", "8"];
+let score = 0;
 for (let i = 0; i < list.length; i++) {
     let card = document.createElement('div');
     card.className = 'cardi';
