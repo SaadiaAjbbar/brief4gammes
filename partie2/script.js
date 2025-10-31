d9b189b8921b76cbbbf973770c0bb81fa35e13aa
SaadiaAjbbar <ajbbarsaadia6@gmail.com>
2025-10-31 08:01:07 +0100

    ecrire le text de input pour chaque charactere dans span

--- partie2/script.js
+++ partie2/script.js
@@ -1,5 +1,18 @@
 const textpara = document.getElementById("textpara");
 const input = document.getElementById("input");
+
 //transformer le text textpara au list des char
 const textInput = "DOM Gamification: Play the DOM";
 const letters = textInput.split("");
+
+function Affiche_textInput() {
+  textpara.innerHTML = "";
+  letters.forEach((char) => {
+    const span = document.createElement("span");
+    span.textContent = char;
+    span.dataset.key = char; 
+    textpara.appendChild(span);
+  });
+}
+
+Affiche_textInput();
