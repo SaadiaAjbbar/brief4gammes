b3aabb1c3437e22813ac0eea116423c99c6bc741
SaadiaAjbbar <ajbbarsaadia6@gmail.com>
2025-10-31 07:58:35 +0100

    utiliser split pour transformer le text au list des characteres

--- partie2/script.js
+++ partie2/script.js
@@ -1,2 +1,5 @@
 const textpara = document.getElementById("textpara");
 const input = document.getElementById("input");
+//transformer le text textpara au list des char
+const textInput = "DOM Gamification: Play the DOM";
+const letters = textInput.split("");
